44e61d17cba3fe1e3c4f236a6b4a2e0ab7804dc8
digikar <digikar@proton.me>
2026-04-03 12:44:34 +0200

    Add darwin build and separate from linux

--- scripts/build_sbcl.sh
+++ scripts/build_sbcl.sh
@@ -23,29 +23,47 @@ pushd contrib/asdf
 ./pull-asdf.sh "${ASDF_VERSION}"
 popd
 
-# Link runtime with goodies and overwrite the original
-export SYS_LIBDIR="/usr/lib/x86_64-linux-gnu"
+UNAME=$(uname -s)
 
-LIBZSTD=${SYS_LIBDIR}/libzstd.a
-# Quick hack, not safe for cross-compiling.
-sed -i "s:-lzstd:$LIBZSTD:" src/runtime/Config.*
+# Link runtime with goodies and overwrite the original
+if [ "$UNAME" == Linux ]; then
+    export SYS_LIBDIR="/usr/lib/x86_64-linux-gnu"
+    LIBZSTD=${SYS_LIBDIR}/libzstd.a
+    # Quick hack, not safe for cross-compiling.
+    sed -i "s:-lzstd:$LIBZSTD:" src/runtime/Config.*
+
+    LIBFIXPOSIX=${CUSTOM_LIBDIR}/libfixposix.a
+    LIBCRYPTO=${SYS_LIBDIR}/libcrypto.a
+    LIBSSL=${SYS_LIBDIR}/libssl.a
+    LIBTLS=${SYS_LIBDIR}/libtls.a
+
+    export WHOLE_ARCHIVES="-Wl,--whole-archive $LIBFIXPOSIX $LIBCRYPTO $LIBSSL $LIBTLS"
+
+elif [ "$UNAME" == Darwin ]; then
+    export SYS_LIBDIR="/opt/homebrew/Cellar"
+    LIBZSTD=${SYS_LIBDIR}/zstd/1.5.7_1/lib/libzstd.a
+    # Quick hack, not safe for cross-compiling.
+    sed -i '' "s:-lzstd:$LIBZSTD:" src/runtime/Config.*
+
+    LIBFIXPOSIX=${CUSTOM_LIBDIR}/libfixposix.a
+    LIBCRYPTO="${SYS_LIBDIR}/openssl@3/${OPENSSL_VERSION}/lib/libcrypto.a"
+    LIBSSL="${SYS_LIBDIR}/openssl@3/${OPENSSL_VERSION}/lib/libssl.a"
+    LIBTLS="${SYS_LIBDIR}/libretls/${LIBTLS_VERSION}/lib/libtls.a"
+
+    # -force_load only works on one library at a time
+    export WHOLE_ARCHIVES="-Wl,-force_load $LIBFIXPOSIX -Wl,-force_load $LIBCRYPTO -Wl,-force_load $LIBSSL -Wl $LIBTLS"
+fi
+
 cp ../scripts/COPYING.zstd ./
 
 env SBCL_MAKE_PARALLEL=1 \
     SBCL_MAKE_JOBS=-j4 \
-    ./make.sh --xc-host="${SBCL_HOST}/run-sbcl.sh --noinform --no-userinit" \
+    ./make.sh --xc-host="${SBCL_HOST} --noinform --no-userinit" \
     --with-sb-core-compression \
     --with-sb-linkable-runtime \
     --without-gencgc --with-mark-region-gc \
     --without-sb-eval \
     --with-sb-fasteval
-
-LIBFIXPOSIX=${CUSTOM_LIBDIR}/libfixposix.a
-LIBCRYPTO=${SYS_LIBDIR}/libcrypto.a
-LIBSSL=${SYS_LIBDIR}/libssl.a
-LIBTLS=${SYS_LIBDIR}/libtls.a
-
-export WHOLE_ARCHIVES="$LIBFIXPOSIX $LIBCRYPTO $LIBSSL $LIBTLS"
 
 make -C src/runtime -f binaries.mk sbcl.extras
 mv -vf src/runtime/sbcl.extras src/runtime/sbcl
@@ -62,8 +80,8 @@ mv -v sbcl "${SRCDIST}"
 "${SRCDIST}"/source-distribution.sh "${SRCDIST}"
 bzip2 "${SRCDIST}"-source.tar
 
-# Build binary x86_64 distribution
-BINDIST="${SRCDIST}"-x86-64-linux
+# Build binary distribution
+BINDIST="${SRCDIST}"-$(uname -m)-$(uname -s | tr '[:upper:]' '[:lower:]')
 mv -v "${SRCDIST}" "${BINDIST}"
 "${BINDIST}"/binary-distribution.sh "${BINDIST}"
 bzip2 "${BINDIST}"-binary.tar
